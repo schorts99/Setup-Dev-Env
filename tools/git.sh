6a351867f29f83898f42fb151e419ac3329a5241
Jorge Castillo <schorts@outlook.com>
2026-01-28 14:50:43 -0600

    feat: Config git

--- tools/git.sh
+++ tools/git.sh
@@ -8,7 +8,11 @@ setup_git() {
   echo "Starting Git installation..."
 
   sudo apt-get install git
+  read -p "Enter your name: " USER_NAME
+  read -p "Enter your email: " USER_EMAIL
   git config --global color.ui true
+  git config --global user.name "$USER_NAME"
+  git config --global user.email "$USER_EMAIL"
 
   echo "✅ Git installed"
 }
